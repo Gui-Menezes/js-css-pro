fe599e425f70cad1ad4fd033f0364a9412053376
Guilherme <guibenmen@gmail.com>
2021-07-05 19:35:03 -0300

    Tentativa de fazer dos fetchs (tem de ser arrumado)

--- Aula19/Busca Filmes 2/js/busca-filme.js
+++ Aula19/Busca Filmes 2/js/busca-filme.js
@@ -1,12 +1,15 @@
 const mostraFilmes = document.querySelector('#btn-buscar');
 const url = 'http://www.omdbapi.com/?s=';
 const apiKey = '&apikey=994f918b';
+const urlMaisInfos ='http://www.omdbapi.com/?t=';
 
 mostraFilmes.addEventListener('click', function(event) {
     event.preventDefault();
     const valorBusca = document.querySelector('#buscador').value;
     const urlBusca = `${url}${valorBusca}${apiKey}`;
+    const urlRestoInfos = `${urlMaisInfos}${valorBusca}${apiKey}`
     buscaFilme(urlBusca);
+    buscaInfos(urlRestoInfos);
 });
 
 // ----GET----
@@ -14,30 +17,56 @@ const buscaFilme = (url) => {
     fetch(url, {
         method: "GET"
     }).then((resposta) => resposta.json())
-    .then((resposta) => mostraArray(resposta));   
+    .then((resposta) => mostraArray(resposta));
+};
+
+const buscaInfos = (url) => {
+    fetch(url, {
+        method: "GET"
+    }).then((response) => response.json())
+    .then((response) => mostraArrayInfos(response))
 };
 // -------------
 
-function mostraArray(resposta) {
-    let saida = '';
-    for(let i = 0; i < resposta.Search.length; i++){
-        const {Poster, Title, Plot, Runtime, Genre, imdbRating} = resposta.Search[i];
-        saida += `<img src="${Poster}" alt="${Title}"/>
-        <div class="titulo">${Title}</div>
-        <div class="descricao">${Plot}.</div>
-        <div class="duracao">Duração: ${Runtime}</div>
-        <div class="genero">Gênero: ${Genre}</div>
-        <div class="nota">Nota: ${imdbRating}</div>`
+function mostraArrayInfos(response) {
+    console.log(response);
+    let saidaInfos = '';
+
+    for(let i = 0; i < response.length; i++) {
+        const {Plot, Runtime, Genre, imdbRating} = response[i];
+
+        saidaInfos = `<p class="ano">${Plot}.</p>
+        <div class="mediaCard">
+            <img src="${Poster}" alt="${Title}
+        </div>
+        <div class="bottomCard">
+            <p class="genero">Gênero: ${Genre}</p>
+            <p class="duracao">Duração: ${Runtime}</p>
+            <p class="nota">Nota: ${imdbRating}</p>
+        </div>`
+
+        document.querySelector('.card').innerHTML = saidaInfos;
     }
-    // console.log(saida);
-    document.querySelector('.filmes').innerHTML = saida;
 }
 
+function mostraArray(resposta) {
+    console.log(resposta);
 
+    let saida = '';
 
+    for(let i = 0; i < resposta.Search.length; i++){
+        const {Poster, Title, Plot, Runtime, Genre, imdbRating} = resposta.Search[i];
 
+        saida += `<div class="content">
+        <div class="card">
+            <div class="topCard">
+                <h2 class="titulo">${Title}</h2>
+            </div>
+        </div>
+    </div>`
 
+        document.querySelector('.info-filmes').innerHTML = saida;
 
+    }
 
-
-
+}
